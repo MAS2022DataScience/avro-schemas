d12d319fbb70f1eb2d1552e35f31c18bf90539ae
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-01-21 19:54:01 +0100

    fix player and team util methods

--- src/main/java/com/mas2022datascience/util/Player.java
+++ src/main/java/com/mas2022datascience/util/Player.java
@@ -1,25 +1,56 @@
 package com.mas2022datascience.util;
 
+import com.mas2022datascience.avro.v1.Object;
 import com.mas2022datascience.avro.v1.TracabGen5TF01Metadata;
 import com.mas2022datascience.avro.v1.TracabGen5TF01Player;
 import java.util.Optional;
 
 public class Player {
 
+  /**
+   * The method returns the player out of the home team and the jersey number.
+   * @param JerseyNumber of type String
+   * @param homeAwayTeamId of type String
+   * @param metadata of type TracabGen5TF01Metadata
+   * @return player id or null (Optional of type String)
+   */
   public static String getPlayerIdJerseyNumber(String JerseyNumber,
       String homeAwayTeamId, TracabGen5TF01Metadata metadata) {
-    Optional<TracabGen5TF01Player> result;
-    if (homeAwayTeamId.equals(String.valueOf(metadata.getHomeTeam().getTeamID()))) {
-      result = metadata.getHomeTeam().getPlayers().stream()
+    Optional<TracabGen5TF01Player> result = null;
+
+    switch (homeAwayTeamId) {
+      case "0":
+        result = metadata.getHomeTeam().getPlayers().stream()
+            .filter(player -> player.getJerseyNo() == Integer.parseInt(JerseyNumber)).findFirst();
+        break;
+      case "1":
+        result = metadata.getAwayTeam().getPlayers().stream()
           .filter(player -> player.getJerseyNo() == Integer.parseInt(JerseyNumber)).findFirst();
-    } else {
-      result = metadata.getAwayTeam().getPlayers().stream()
-          .filter(player -> player.getJerseyNo() == Integer.parseInt(JerseyNumber)).findFirst();
+        break;
+      default:
+        result = Optional.empty();
     }
+
     if (result.isPresent()) {
       return String.valueOf(result.get().getPlayerID());
     } else {
       return null;
     }
   }
+
+  /**
+   * The method returns the player id or the ball id (0)
+   * @param playerObject
+   * @return
+   */
+  public static String getPlayerOrBallId(Object playerObject) {
+    if (playerObject.getType() == 0 || playerObject.getType() == 1) {
+      // Player
+      return playerObject.getPlayerId();
+    } else {
+      // Referee
+      return String.valueOf(playerObject.getType());
+    }
+  }
+
 }
--- src/main/java/com/mas2022datascience/util/Team.java
+++ src/main/java/com/mas2022datascience/util/Team.java
@@ -1,15 +1,41 @@
 package com.mas2022datascience.util;
 
+import com.mas2022datascience.avro.v1.Object;
 import com.mas2022datascience.avro.v1.TracabGen5TF01Metadata;
 
 public class Team {
 
+  /**
+   * The method returns the team id out of the home/away team ID.
+   * @param homeAwayTeamId of type Integer
+   * @param metadata of type TracabGen5TF01Metadata
+   * @return team id of type String
+   */
   public static String getTeamIdFromHomeOrAwayId(Integer homeAwayTeamId,
       TracabGen5TF01Metadata metadata) {
     if (homeAwayTeamId == 0) {
       return String.valueOf(metadata.getHomeTeam().getTeamID());
     } else {
-      return String.valueOf(metadata.getAwayTeam().getTeamID());
+      if (homeAwayTeamId == 1) {
+        return String.valueOf(metadata.getAwayTeam().getTeamID());
+      } else {
+        return null;
+      }
+    }
+  }
+
+  /**
+   * The method returns the team id or "0" if it is the ball.
+   * @param playerObject of type Object
+   * @return team id or "0" (ball) of type String
+   */
+  public static String getTeamId(Object playerObject) {
+    if (playerObject.getType() == 0 || playerObject.getType() == 1) {
+      // Player
+      return playerObject.getTeamId();
+    } else {
+      // Referee or ball
+      return null;
     }
   }
 }
