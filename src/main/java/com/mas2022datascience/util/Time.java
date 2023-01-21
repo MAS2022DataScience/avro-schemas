b89b964dd990b049db1dd793c851f62f722bbcfe
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-01-21 20:06:14 +0100

    add getUTCStringFromOffsetValue method

--- src/main/java/com/mas2022datascience/util/Time.java
+++ src/main/java/com/mas2022datascience/util/Time.java
@@ -18,4 +18,25 @@ public class Time {
     return Instant.from(fmt.parse(utcString)).toEpochMilli();
   }
 
+  /**
+   * Calculates the UTC String out of the offset value and the initial time
+   * @param offset
+   * @param frameRate
+   * @param initialFrameNumber
+   * @param initialTime
+   * @return UTC String
+   */
+  public static String getUTCStringFromOffsetValue(long offset , long frameRate, long initialFrameNumber,
+      String initialTime) {
+    if (offset == 0) {
+      return null;
+    } else {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+      return Instant.ofEpochMilli(Instant.parse(initialTime).toEpochMilli() +
+          (offset - initialFrameNumber) *
+              (1000 / frameRate)).atZone(ZoneOffset.UTC).format(formatter).toString();
+    }
+  }
+
 }
